7a53fa2dec285d49898e8cf1b7ab576977994386
Xiejinhua123 <30788091+Xiejinhua123@users.noreply.github.com>
2019-04-12 14:10:28 +0800

    测试test分支信息

--- zookeeperTest-common/src/main/java/com/zookeeper/service/StudentService.java
+++ zookeeperTest-common/src/main/java/com/zookeeper/service/StudentService.java
@@ -3,11 +3,28 @@ package com.zookeeper.service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * @author  xiejinhua
+ */
 public interface StudentService {
 
+	/**
+	 * 执行登陆
+	 * @return
+	 * 		执行结果
+	 * @throws Exception
+	 * 		可能出现的异常信息
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
     public  String login() throws  Exception;
-	
+
+	/**
+	 * 获取同一个学生细腻
+	 * @return
+	 * 		执行结果
+	 * @throws Exception
+	 * 		可能出现的异常信息
+	 */
 	@RequestMapping(value = "/getStucent", method = RequestMethod.GET)
 	public String getStudent() throws Exception;
 	
---
+++ zookeeperTest-common/src/main/java/com/zookeeper/service/TestService.java
@@ -0,0 +1,16 @@
+package com.zookeeper.service;
+
+/**
+ * @author xiejinhua
+ * @version wu
+ * @date 2019-04-12
+ * @update xiejinhua
+ * @description 测试类
+ */
+public class TestService {
+
+    public static void main ( String[] args ){
+        System.out.print("123456");
+    }
+
+}
